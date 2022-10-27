d0aaf45bf10f6d12bd1585995f9aa158762de513
Andrea Severini <andreaseverini94@gmail.com>
2022-10-27 18:25:49 +0200

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -12,6 +12,17 @@ app.listen(3000, () => {
   console.log("Express Application started on port 3000");
 });
 
+app.get("/todos", (req, res) => {
+  Todo.find().then(
+    (todos) => {
+      res.send({ todos });
+    },
+    (e) => {
+      res.status(400).send(e);
+    }
+  );
+});
+
 //To send JSON to the application
 app.use(bodyParser.json());
 
