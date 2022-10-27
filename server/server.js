631500dfd75300d045e9c8cf70951fdbf2ed53bf
Andrea Severini <andreaseverini94@gmail.com>
2022-10-27 16:58:14 +0200

    add POST /todos route and refactor mongose

--- server/server.js
+++ server/server.js
@@ -1,16 +1,43 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
 const { mongoose } = require("./db/mongoose");
 const { Todo } = require("./models/todo");
 const { User } = require("./models/user");
 
-const newTodo = new Todo({
-  text: "Cook dinner",
+//Express Application initialization
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Express Application started on port 3000");
 });
 
-const otherTodo = new Todo({
-  text: "Feed the cat",
-  completed: true,
-  completedAt: 123,
+//To send JSON to the application
+app.use(bodyParser.json());
+
+app.post("/todos", (req, res) => {
+  const todo = new Todo({
+    text: req.body.text,
+  });
+  todo.save().then(
+    (doc) => {
+      res.send(doc);
+    },
+    (e) => {
+      res.status(400).send(e);
+    }
+  );
 });
+
+// const newTodo = new Todo({
+//   text: "Cook dinner",
+// });
+
+// const otherTodo = new Todo({
+//   text: "Feed the cat",
+//   completed: true,
+//   completedAt: 123,
+// });
 
 //Calling a method to update the database
 // otherTodo.save().then(
@@ -36,29 +63,29 @@ const otherTodo = new Todo({
 //   }
 // );
 
-const newUserNoEmail = new User({});
-const newUser = new User({
-  email: "andreaseverini94@gmail.com",
-});
+// const newUserNoEmail = new User({});
+// const newUser = new User({
+//   email: "andreaseverini94@gmail.com",
+// });
 
-newUserNoEmail.save().then(
-  (user) => {
-    console.log("------THE USER HAS BEEN CREATED------");
-    console.log(JSON.stringify(user, undefined, 2));
-  },
-  (e) => {
-    console.log("------UNABLE TO CREATE USER------");
-    console.log(e);
-  }
-);
+// newUserNoEmail.save().then(
+//   (user) => {
+//     console.log("------THE USER HAS BEEN CREATED------");
+//     console.log(JSON.stringify(user, undefined, 2));
+//   },
+//   (e) => {
+//     console.log("------UNABLE TO CREATE USER------");
+//     console.log(e);
+//   }
+// );
 
-newUser.save().then(
-  (user) => {
-    console.log("------THE USER HAS BEEN CREATED------");
-    console.log(JSON.stringify(user, undefined, 2));
-  },
-  (e) => {
-    console.log("------UNABLE TO CREATE USER------");
-    console.log(e);
-  }
-);
+// newUser.save().then(
+//   (user) => {
+//     console.log("------THE USER HAS BEEN CREATED------");
+//     console.log(JSON.stringify(user, undefined, 2));
+//   },
+//   (e) => {
+//     console.log("------UNABLE TO CREATE USER------");
+//     console.log(e);
+//   }
+// );
